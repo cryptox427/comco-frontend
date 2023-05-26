31743bf0e56b0fb91e8a176add3c68b6f09fa5c8
cryptox427 <smithzhang0427@gmail.com>
2023-05-26 08:28:08 -0400

    upload

---
+++ src/assets/data/about/aboutv1.js
@@ -0,0 +1,27 @@
+import aboutCardIcon1 from "../../images/icon/aboutus_card_icon1.svg"
+import aboutCardIcon2 from "../../images/icon/aboutus_card_icon2.svg"
+import aboutCardIcon3 from "../../images/icon/aboutus_card_icon3.svg"
+
+ const data = {
+    aboutDescription1: 'Welcome to Comedy Coins, a new form of digital currency deployed by TheSoFunny, LLC. For nearly 20 years, TheSoFunny, LLC has been at the forefront of comedy show production, delivering laughs and entertainment to millions worldwide. Now, we are embarking on a new journey to further connect with our audience and reshape how you interact with comedy. This whitepaper aims to elucidate the vision, technology, and mechanics behind Comedy Coins.',
+    aboutDescription2: 'Our mission is to use blockchain technology to democratize access to comedy and foster a more engaging, interactive experience for our fans. Comedy Coins serves as a digital currency that can be used to purchase tickets for shows, seminars, comedy festivals, and exclusive non-fungible tokens (NFTs) produced by TheSoFunny, LLC, and its partner organizations.',
+    aboutInfo: [
+        {
+            icon: aboutCardIcon1,
+            title: "Purchasing Power",
+            text: "Comedy Coinss can be used to purchase tickets to comedy shows, seminars, festivals, and exclusive NFTs."
+        },
+        {
+            icon: aboutCardIcon2,
+            title: "Rewards and Incentives",
+            text: 'Holders of Comedy Coinss may be eligible for special rewards, like early access to ticket sales, VIP experiences, and exclusive content.'
+        },
+        {
+            icon: aboutCardIcon3,
+            title: "Community Engagement",
+            text: 'Future plans include opportunities for Comedy Coins holders to participate in governance decisions or special events.'
+        }
+    ]
+}
+
+export default data;
---
+++ src/assets/data/roadMap/roadMapV1.js
@@ -0,0 +1,73 @@
+import checkIcon from "../../images/icon/roadmap_check.svg"
+import spinnerIcon from "../../images/icon/roadmap_spiner.png"
+
+const data = [
+    {
+        icon: checkIcon,
+        title: "Phase 01 ",
+        subtitle: "Pre-Launch (Q2 2023)",
+        text: "Conduct thorough smart contract audits: Prioritize security by conducting comprehensive audits of the Comedy Coins smart contract to ensure the safety of funds and the integrity of the system. Establish Social media presence as it relates to comedy and the world of Crypto."
+    },
+    {
+        icon: checkIcon,
+        title: "Phase 02",
+        subtitle: "Token Utilization and Expansion (Q3 2023)",
+        text: "Enable Comedy Coins for ticket purchases: Integrate Comedy Coins into TheSoFunny, LLC's ticketing system, allowing users to purchase tickets for comedy shows, seminars, festivals, and other events using Comedy Coins."
+    },
+    {
+        icon: checkIcon,
+        title: "Phase 03",
+        subtitle: "Ecosystem Expansion (Q4 2023)",
+        text: "Forge partnerships with industry players: Actively engage with other organizations and companies in the comedy and entertainment industry to expand the utility and reach of Comedy Coins. Announce partnerships through official communication channels to raise awareness and broaden the user base."
+    },
+    {
+        icon: spinnerIcon,
+        title: "Phase 04",
+        subtitle: "Integration with Online Comedy Platforms",
+        text: "Explore partnerships and integration opportunities with online comedy platforms, streaming services, and content distribution networks. By integrating Comedy Coins into these platforms, users can access comedy- related content, shows, and live-streamed events, creating additional utility and demand for Comedy Coins"
+    },
+    {
+        icon: spinnerIcon,
+        title: "Phase 05",
+        subtitle: "Community Governance (Q1 2024 and Beyond)",
+        text: "Implement community governance features: Further develop and integrate community governance mechanisms to give Comedy Coins holders a voice in decision-making processes and the ability to shape the future direction of the project."
+    },
+    {
+        icon: spinnerIcon,
+        title: "Phase 06",
+        subtitle: "Partnerships with Comedy Clubs and Venues",
+        text: "Collaborate with established comedy clubs and venues to integrate Comedy Coins into their ticketing systems and event promotions. This partnership can expand the adoption of Comedy Coins as a preferred digital currency for comedy-related events and further strengthen the project's position within the comedy industry."
+    },
+    {
+        icon: spinnerIcon,
+        title: "Phase 07",
+        subtitle: "Metaverse Expansion and Entertainment Productions (Beyond Q1 2024)",
+        text: "Metaverse Comedy Club: Explore the integration of Comedy Coins into a virtual comedy club within the metaverse. This virtual venue would allow users to attend live comedy shows, interact with comedians, and purchase tickets using Comedy Coins. Create an immersive and engaging experience that leverages the possibilities of virtual reality and blockchain technology."
+    },
+    {
+        icon: spinnerIcon,
+        title: "Phase 08",
+        subtitle: "Expansion to Other Blockchain Networks",
+        text: "Explore opportunities to expand Comedy Coins beyond the Polygon (Matic) Network and integrate with other compatible blockchain networks. This can enhance interoperability, reach a broader user base, and provide additional scalability options for the project."
+    },
+    {
+        icon: spinnerIcon,
+        title: "Phase 09",
+        subtitle: "Enhanced Community Features",
+        text: "Continuously improve the community features within the Comedy Coins ecosystem. This can include the development of a robust community forum, social networking capabilities, and interactive features that encourage user engagement and collaboration."
+    },
+    {
+        icon: spinnerIcon,
+        title: "Phase 10",
+        subtitle: "Enhanced Governance and Voting Mechanisms",
+        text: "Continuously iterate and enhance the governance and voting mechanisms within the Comedy Coins ecosystem. This can involve introducing staking mechanisms for voting rights, enabling community-driven proposals, and fostering transparent decision-making processes that involve Comedy Coins holders."
+    },
+    {
+        icon: spinnerIcon,
+        title: "Phase 11",
+        subtitle: "Enhanced Security  Measures and Auditing",
+        text: "Implement ongoing security measures to protect user funds and maintain the integrity of the Comedy Coins system. Regular security audits should be conducted by reputable third-party auditors to identify and mitigate potential vulnerabilities and ensure a high level of security for users."
+    }
+]
+
+export default data;
